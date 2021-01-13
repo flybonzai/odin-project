9b0f57f966b512863552cce80188e8e72b82a431
mcharris <mcharris@adobe.com>
2021-01-13 10:06:38 -0700

    mootube

---
+++ mootube/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">
+
+      </nav>
+      <section className="content">
+        <section className="media-viewport">
+          <section className="player">
+
+          </section>
+          <section className="title-bar">
+
+          </section>
+          <section className="comments">
+
+          </section>
+
+        </section>
+      </section>
+
+      <aside className="thumbnails">
+
+      </aside>
+    </div>
+  );
+}
+
+export default App;
